668d82dc89a92e1b71bcf2bcfa6bd0893ccad4f8
Lucio Berté <lucioaberte@gmail.com>
2023-10-03 12:54:01 -0300

    update minifolio

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -22,13 +22,13 @@ const Header = () => {
                             Lucio Berté
                         </h2>
                         <h3 className="profile__profession animate__animated animate__fadeInDown animate__delay-0.7s"> 
-                            Junior FullStack Developer
+                            FullStack Developer
                         </h3>
                         <ul className="profile__social animate__animated animate__fadeInDown animate__delay-0.8s">
-                            <a href="https://portfolio-lucioaberte.vercel.app/" target="_blank" className="profile__social-link">
+                            <a href="https://minifolio-lucioaberte.vercel.app/" target="_blank" className="profile__social-link">
                                 <i className="ri-home-4-line"></i>
                             </a>
-                            <a href="https://www.linkedin.com/in/lucioabert%C3%A9/" target="_blank" className="profile__social-link">
+                            <a href="https://www.linkedin.com/in/lucioaberte/" target="_blank" className="profile__social-link">
                                 <i className="ri-linkedin-box-line"></i>
                             </a>
                             <a href="https://github.com/LucioABerte" target="_blank" className="profile__social-link">
@@ -39,19 +39,19 @@ const Header = () => {
               
                     <div className="profile__info grid animate__animated animate__fadeInLeft animate__delay-0.9s">
                         <div className="profile__info-group">
-                            <h3 className="profile__info-number">+1</h3>
+                            <h3 className="profile__info-number">+2</h3>
                             <p className="profile__info-description">
-                                Years of <br/> work
+                                Years of <br/> experience
                             </p>
                         </div>
                         <div className="profile__info-group">
-                            <h3 className="profile__info-number">+8</h3>
+                            <h3 className="profile__info-number">+2</h3>
                             <p className="profile__info-description">
-                                completed <br/> projects
+                                Years with <br/> MERN
                             </p>
                         </div>
                         <div className="profile__info-group">
-                            <h3 className="profile__info-number">+1</h3>
+                            <h3 className="profile__info-number">+2</h3>
                             <p className="profile__info-description">
                                 Specializing in <br/> ReactJS
                             </p>
@@ -60,15 +60,15 @@ const Header = () => {
                          
                     <div className="profile__buttons">
 
-                            <a download="" href="LucioCv.pdf" target="_blank" className="button animate__animated animate__fadeInLeft animate__delay-09s">
+                            <a download="" href="LucioBertéCV(English).pdf" target="_blank" className="button animate__animated animate__fadeInLeft animate__delay-09s">
                                 Download CV <i className="ri-download-2-line"></i>
                             </a>
 
                         <div className="profile__buttons-small animate__animated animate__fadeInRight animate__delay-1.1s">
-                            <a href="https://api.whatsapp.com/send/?phone=5492235585610&text&type=phone_number&app_absent=0" target="_blank" className="button button__small button__gray">
+                            <a href="https://api.whatsapp.com/send/?phone=5492233479633&text&type=phone_number&app_absent=0" target="_blank" className="button button__small button__gray">
                                 <i className="ri-whatsapp-line"></i>
                             </a>
-                            <a href="https://www.linkedin.com/in/lucioabert%C3%A9/" target="_blank" className="button button__small button__gray">
+                            <a href="https://www.linkedin.com/in/lucioaberte/" target="_blank" className="button button__small button__gray">
                                 <i className="ri-messenger-line"></i>
                             </a>
                         </div>
--- src/components/tabs/ProjectTab.jsx
+++ src/components/tabs/ProjectTab.jsx
@@ -4,34 +4,35 @@ import "./tabs.css";
 const FirstTab = () => {
   return (
         <div className="projects__content grid filters__active">
-                <article className="projects__card animate__animated animate__fadeInRight animate__delay-0.5s">
-                    <img src="wedding.png" alt="img" className="projects__img"/>
+
+                <article className="projects__card animate__animated animate__fadeInLeft animate__delay-0.6s">
+                    <img src="CalendarApp.jpg" alt="img" className="projects__img" />
 
                     <div className="projects__modal">
                         <div>
-                            <span className="projects__subtitle"> Sidebar App</span>
-                            <h3 className="projects__title">Wedding App</h3>
-                            <a href="https://jazminylucio.netlify.app/" target="_blank" className="projects__button button button__small">
+                            <span className="projects__subtitle"> MERN App </span>
+                            <h3 className="projects__title">Calendar App | Client-Server side</h3>
+                            <a href="https://calendar-app-lucioaberte.vercel.app" target="_blank" className="projects__button button button__small">
                                 <i className="ri-link"></i>
                             </a>
-                            <a href="https://github.com/LucioABerte/Wedding" target="_blank" className="projects__button button button__small two">
+                            <a href="https://github.com/LucioABerte/CalendarApp" target="_blank" className="projects__button button button__small two">
                                 <i className="ri-code-line"></i>
                             </a>
                         </div>
                     </div>
                 </article>
 
-                <article className="projects__card animate__animated animate__fadeInLeft animate__delay-0.6s">
-                    <img src="portfolio.png" alt="img" className="projects__img" />
+                <article className="projects__card animate__animated animate__fadeInLeft animate__delay-0.8s">
+                    <img src="JournalApp.jpg" alt="img" className="projects__img"/>
 
                     <div className="projects__modal">
                         <div>
-                            <span className="projects__subtitle"> React App </span>
-                            <h3 className="projects__title">Personal Portfolio</h3>
-                            <a href="https://portfolio-lucioaberte.vercel.app/" target="_blank" className="projects__button button button__small">
+                            <span className="projects__subtitle">MERN App</span>
+                            <h3 className="projects__title">Firebase Journal CRUD</h3>
+                            <a href="https://journal-crud.vercel.app/" target="_blank" className="projects__button button button__small">
                                 <i className="ri-link"></i>
                             </a>
-                            <a href="https://github.com/LucioABerte/Portfolio" target="_blank" className="projects__button button button__small two">
+                            <a href="https://github.com/LucioABerte/JournalCrud" target="_blank" className="projects__button button button__small two">
                                 <i className="ri-code-line"></i>
                             </a>
                         </div>
@@ -55,17 +56,34 @@ const FirstTab = () => {
                     </div>
                 </article>
 
-                <article className="projects__card animate__animated animate__fadeInLeft animate__delay-0.8s">
-                    <img src="meme.png" alt="img" className="projects__img"/>
+                <article className="projects__card animate__animated animate__fadeInRight animate__delay-0.5s">
+                    <img src="wedding.png" alt="img" className="projects__img"/>
 
                     <div className="projects__modal">
                         <div>
-                            <span className="projects__subtitle">React App</span>
-                            <h3 className="projects__title">Meme Generator</h3>
-                            <a href="https://meme-generator-lucioaberte.vercel.app/" target="_blank" className="projects__button button button__small">
+                            <span className="projects__subtitle"> Sidebar App</span>
+                            <h3 className="projects__title">Wedding App</h3>
+                            <a href="https://jazminylucio.netlify.app/" target="_blank" className="projects__button button button__small">
                                 <i className="ri-link"></i>
                             </a>
-                            <a href="https://github.com/LucioABerte/MemeGenerator" target="_blank" className="projects__button button button__small two">
+                            <a href="https://github.com/LucioABerte/Wedding" target="_blank" className="projects__button button button__small two">
+                                <i className="ri-code-line"></i>
+                            </a>
+                        </div>
+                    </div>
+                </article>
+
+                <article className="projects__card animate__animated animate__fadeInLeft animate__delay-1s">
+                    <img src="tesla.png" alt="img" className="projects__img"/>
+
+                    <div className="projects__modal">
+                        <div>
+                            <span className="projects__subtitle">React Web App</span>
+                            <h3 className="projects__title">Tesla Clone Website</h3>
+                            <a href="https://tesla-clone-lucioaberte.vercel.app/#" target="_blank" className="projects__button button button__small">
+                                <i className="ri-link"></i>
+                            </a>
+                            <a href="https://github.com/LucioABerte/TravelApp" target="_blank" className="projects__button button button__small two">
                                 <i className="ri-code-line"></i>
                             </a>
                         </div>
@@ -89,22 +107,6 @@ const FirstTab = () => {
                     </div>
                 </article>
 
-                <article className="projects__card animate__animated animate__fadeInLeft animate__delay-1s">
-                    <img src="travel.png" alt="img" className="projects__img"/>
-
-                    <div className="projects__modal">
-                        <div>
-                            <span className="projects__subtitle">Web App</span>
-                            <h3 className="projects__title">My Travel Journal</h3>
-                            <a href="https://travel-app-lucioaberte.vercel.app/" target="_blank" className="projects__button button button__small">
-                                <i className="ri-link"></i>
-                            </a>
-                            <a href="https://github.com/LucioABerte/TravelApp" target="_blank" className="projects__button button button__small two">
-                                <i className="ri-code-line"></i>
-                            </a>
-                        </div>
-                    </div>
-                </article>
         </div>
   );
 };
